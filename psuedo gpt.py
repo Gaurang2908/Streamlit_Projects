5af672add8464c417122bc5084d49c4c2579ca1c
Gaurang Santoria <72335713+Gaurang2908@users.noreply.github.com>
2025-06-17 15:28:08 +0530

    Update psuedo gpt.py

--- psuedo gpt.py
+++ psuedo gpt.py
@@ -4,7 +4,7 @@ import openai
 st.title("Pseudo ChatGPT")
 
 openai.api_key = st.secrets["openai"]["api_key"]
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
