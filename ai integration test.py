eaeaff97b3d2df31e0c77d8c9fe9da9e38c6483b
Gaurang Santoria <72335713+Gaurang2908@users.noreply.github.com>
2025-06-17 11:57:55 +0530

    Update ai integration test.py

--- ai integration test.py
+++ ai integration test.py
@@ -4,3 +4,7 @@ import numpy as np
 with st.chat_message("assistant"):
     st.write("Hello human")
     st.bar_chart(np.random.randn(30, 3))
+
+prompt = st.chat_input("Type here")
+if prompt :
+    st.write(f"User has sent the following prompt:{prompt}")
