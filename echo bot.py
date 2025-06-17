73a62998c40d3e68198d5cefce297bb33d438111
Gaurang Santoria <72335713+Gaurang2908@users.noreply.github.com>
2025-06-17 12:29:34 +0530

    Update and rename ai integration test.py to echo bot.py

---
+++ echo bot.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import numpy as np
+
+with st.chat_message("assistant", avatar="🤖"):
+    st.write("Hello human")
+    st.line_chart(np.random.randn(40, 4))
+
+prompt = st.chat_input("Write something")
+if prompt :
+    st.write(f"User has sent the following prompt: {prompt}")
+
+st.tile("Echo Bot")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What's up?"):
+    with st.chat_message("user", avatar="🙇‍♂️"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role":"user", "content": prompt})
+
+response = f"Echo: {prompt}"
+with st.chat_message("assistant"):
+    st.markdown(response)
+st.session_state.messages.append({"role": "assistant", "content": response})
